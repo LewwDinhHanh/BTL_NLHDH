bb52c15cffab26b5fecc20342687426011289f02
LewwDinhHanh <hanhkk0909@gmail.com>
2024-05-02 23:00:52 +0700

    first commit

---
+++ BTL_HĐH/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿
+namespace BTL_HĐH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Word = new System.Windows.Forms.Button();
+            this.Excel = new System.Windows.Forms.Button();
+            this.Powp = new System.Windows.Forms.Button();
+            this.CloseApp = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "winword",
+            "excel",
+            "POWERPNT"});
+            this.comboBox1.Location = new System.Drawing.Point(67, 166);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 34);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Word
+            // 
+            this.Word.BackColor = System.Drawing.Color.Chartreuse;
+            this.Word.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Word.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Word.Location = new System.Drawing.Point(67, 52);
+            this.Word.Margin = new System.Windows.Forms.Padding(4);
+            this.Word.Name = "Word";
+            this.Word.Size = new System.Drawing.Size(100, 44);
+            this.Word.TabIndex = 1;
+            this.Word.Text = "Word";
+            this.Word.UseVisualStyleBackColor = false;
+            this.Word.Click += new System.EventHandler(this.Word_Click);
+            // 
+            // Excel
+            // 
+            this.Excel.BackColor = System.Drawing.Color.Chartreuse;
+            this.Excel.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Excel.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Excel.Location = new System.Drawing.Point(192, 52);
+            this.Excel.Margin = new System.Windows.Forms.Padding(4);
+            this.Excel.Name = "Excel";
+            this.Excel.Size = new System.Drawing.Size(100, 44);
+            this.Excel.TabIndex = 1;
+            this.Excel.Text = "Excel";
+            this.Excel.UseVisualStyleBackColor = false;
+            this.Excel.Click += new System.EventHandler(this.Excel_Click);
+            // 
+            // Powp
+            // 
+            this.Powp.BackColor = System.Drawing.Color.Chartreuse;
+            this.Powp.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Powp.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Powp.Location = new System.Drawing.Point(309, 52);
+            this.Powp.Margin = new System.Windows.Forms.Padding(4);
+            this.Powp.Name = "Powp";
+            this.Powp.Size = new System.Drawing.Size(100, 44);
+            this.Powp.TabIndex = 1;
+            this.Powp.Text = "PowerPoint";
+            this.Powp.UseVisualStyleBackColor = false;
+            this.Powp.Click += new System.EventHandler(this.Powp_Click);
+            // 
+            // CloseApp
+            // 
+            this.CloseApp.BackColor = System.Drawing.Color.Red;
+            this.CloseApp.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseApp.Location = new System.Drawing.Point(309, 160);
+            this.CloseApp.Margin = new System.Windows.Forms.Padding(4);
+            this.CloseApp.Name = "CloseApp";
+            this.CloseApp.Size = new System.Drawing.Size(100, 44);
+            this.CloseApp.TabIndex = 3;
+            this.CloseApp.Text = "End Task";
+            this.CloseApp.UseVisualStyleBackColor = false;
+            this.CloseApp.Click += new System.EventHandler(this.CloseApp_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 242);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.MediumBlue,
+        System.Drawing.Color.Green,
+        System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))))};
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "word";
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series2.BorderWidth = 2;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "excel";
+            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+            series3.BorderWidth = 2;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.Name = "pp";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(607, 290);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            title1.Alignment = System.Drawing.ContentAlignment.TopLeft;
+            title1.Name = "Title1";
+            title1.Text = "MB";
+            title2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            title2.Name = "Title2";
+            title2.Text = "Giây";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Titles.Add(title2);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(516, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Total:";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(617, 0);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(604, 532);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Process Name";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PID";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Memory ( Private WorkingSet )";
+            this.columnHeader4.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1091, 575);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 45);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "End Task";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbCount
+            // 
+            this.lbCount.AutoSize = true;
+            this.lbCount.Font = new System.Drawing.Font("Microsoft Uighur", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCount.Location = new System.Drawing.Point(560, 15);
+            this.lbCount.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCount.Name = "lbCount";
+            this.lbCount.Size = new System.Drawing.Size(20, 26);
+            this.lbCount.TabIndex = 6;
+            this.lbCount.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1223, 638);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lbCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.CloseApp);
+            this.Controls.Add(this.Powp);
+            this.Controls.Add(this.Excel);
+            this.Controls.Add(this.Word);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Word;
+        private System.Windows.Forms.Button Excel;
+        private System.Windows.Forms.Button Powp;
+        private System.Windows.Forms.Button CloseApp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbCount;
+    }
+}
+
---
+++ BTL_HĐH/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics; // Cung cấp các lớp cho phép tương tác với các process, event log và performance counters
+namespace BTL_HĐH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+         
+        }
+        private void Word_Click(object sender, EventArgs e) // sự kiện khi bấm vào nút word
+        {
+            Process.Start("winword"); // khởi động chương trình có tên file là winword
+        }
+        private void Excel_Click(object sender, EventArgs e) // sự kiện khi bấm vào nút excel
+        {
+            Process.Start("excel"); // khởi động chương trình có tên file là excel
+         
+        }
+
+        private void Powp_Click(object sender, EventArgs e)// sự kiện khi bấm vào nút Powp
+        {
+            Process.Start("POWERPNT"); // khởi động chương trình có tên file là powerpnt
+        }
+
+
+        private void CloseApp_Click(object sender, EventArgs e)// sự kiện khi bấm vào nút CloseApp
+        {
+            foreach(var process in Process.GetProcessesByName(comboBox1.Text)) // vòng lặp foreach chạy mảng các danh sách có tên là comboBox1.Text
+            {
+                process.Kill(); // dừng tiến trình
+            }
+        }
+        public double ByteToMB(long num)// hàm chuyển đổi từ byte sang MB
+        {
+            return (num / 1024 / 1024);// để chuyển từ byte sang MB ta phải chia cho 2 lần 1024 ,lần đầu chia để lấy ra số Kb lần 2 chia ra để lấy MB
+        }
+        Process[] process; // tạo mảng process có kiểu là Process
+        void Load1() // tạo hàm Load1
+        {
+            int count = Convert.ToInt32(lbCount.Text); // chuyển lbCount từ dạng string sang dạng int và gắn cho biến count
+            process = Process.GetProcesses(); // process được gắn bằng tất cả các process đang chạy trong máy
+            if (count != process.Length) // so sánh số lượng count và số lượng chiều dài của mảng process
+            {
+                listView1.Items.Clear(); // xoá hết các item có trong listView1
+                foreach (var x in process) // chạy vòng lặp foreach với mảng là process
+                {
+                    string status = x.Responding == true ? "Running" : "Not Responding"; // gắn cho biến status 1 trong 2 trạng thái running hoặc not responding
+                    ListViewItem newItem = new ListViewItem() // tạo 1 listviewitem mới có biến là new item
+                    {
+                        Text = x.ProcessName // gắn Text bằng tên của process
+                    };
+                    newItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = x.Id.ToString() }); // thêm  item con gắn text bằng id của process
+                    newItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = status }); // thêm item con gắn text bằng trạng thái của process 
+                    newItem.SubItems.Add(new ListViewItem.ListViewSubItem() { Text = ByteToMB(x.WorkingSet64).ToString() + " MB" }); // thêm item con gắn text bằng số MB của process
+                    listView1.Items.Add(newItem); // thêm newIteam vào listView
+
+                }
+                lbCount.Text = process.Length.ToString(); // gắn lbCount  bằng chiều dài của mảng process
+
+            }
+        }
+        void load2() // tạo hàm load 2
+        {
+            long sum = 0; // khỏi tạo biến sum bằng 0
+            Process[] word = Process.GetProcessesByName("winword"); // tạo mảng word gắn mảng với những Process có tên là winword
+            foreach(var x in word) // chạy vòng lặp trong mảng word
+            {
+                sum += x.WorkingSet64; // tổng lượng ram vật lý của mảng word
+
+            }
+            chart1.Series["word"].Points.AddY(ByteToMB(sum)); // gắn điểm của đường word bằng tổng lượng ram vật lý của process có tên là winword đang sử dụng, có sử dụng hàm biến đổi từ byte sang MB
+            Process[] excel = Process.GetProcessesByName("excel"); //tạo mảng excel gắn mảng với những Process có tên là excel
+            sum = 0; // gắn biến sum bằng 0 
+            foreach (var x in excel)// chạy vòng lặp trong mảng word
+            {
+                sum += x.WorkingSet64;// tổng lượng ram vật lý của mảng excel
+
+            }
+            chart1.Series["excel"].Points.AddY(ByteToMB(sum));// gắn điểm của đường excel bằng tổng lượng ram vật lý của process có tên là excel đang sử dụng, có sử dụng hàm biến đổi từ byte sang MB
+            Process[] pp = Process.GetProcessesByName("POWERPNT");//tạo mảng pp gắn mảng với những Process có tên là POWERPNT
+            sum = 0;// gắn biến sum bằng 0 
+            foreach (var x in pp)// chạy vòng lặp trong mảng pp
+            {
+                sum += x.WorkingSet64;// tổng lượng ram vật lý của mảng pp
+
+            }
+            chart1.Series["pp"].Points.AddY(ByteToMB(sum)); // gắn điểm của đường pp bằng tổng lượng ram vật lý của process có tên là POWERPNT đang sử dụng, có sử dụng hàm biến đổi từ byte sang MB
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // gọi hàm Form1 khi Load 
+        {
+            Load1();// gọi hàm load1
+            load2();// gọi hàm load 2
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)// Cứ 1s gọi hàm timer1
+        {
+            load2(); // gọi hàm load 2
+            
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)// Cứ 0,1s gọi hàm timer2
+        {
+            Load1(); // gọi hàm load 1
+            
+        }
+        private void button1_Click(object sender, EventArgs e) // sự kiện khi bấm nút button1 tức là nút End Task
+        {
+            process[listView1.SelectedIndices[0]].Kill(); // dừng tiến trình với item đang chọn trên listView1
+        }
+
+
+        private void listView1_Click(object sender, EventArgs e) // sự kiện khi bấm trên listView1
+        {
+            comboBox1.Text = listView1.SelectedItems[0].Text; // gắn Text của comboBox bằng item đang chọn trên listView
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
